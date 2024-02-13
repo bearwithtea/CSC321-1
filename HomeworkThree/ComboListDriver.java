81b46c8046799f085dd629d6a4aeb0e243741d94
Owen McGrath <132114816+bearwithtea@users.noreply.github.com>
2024-02-13 09:47:30 -0600

    Add files via upload

---
+++ HomeworkThree/ComboListDriver.java
@@ -0,0 +1,11 @@
+public class ComboListDriver {
+    public static void main(String[] args) throws Exception {
+
+        ComboList<Integer> combolist = new ComboList<>(0);
+
+        combolist.add(1);
+        combolist.add(2);
+        combolist.add(3);
+        
+    }
+}
