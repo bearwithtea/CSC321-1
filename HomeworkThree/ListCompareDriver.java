eee651be8ed7abe63074322e052a73b96a330c0b
bearwithtea <132114816+bearwithtea@users.noreply.github.com>
2024-02-20 14:40:46 -0600

    imrpvoed ux

--- HomeworkThree/ListCompareDriver.java
+++ HomeworkThree/ListCompareDriver.java
@@ -5,74 +5,45 @@ import java.util.Scanner;
 
 public class ListCompareDriver {
 
-//add error checking :)
     private static Scanner scanner = new Scanner(System.in);
     private static StopWatch stopWatch = new StopWatch();
 
     public static void main(String[] args) throws Exception {
 
-        String userInput = getsUserInput();
-        Integer getsStartingSize = getsStartingSize();
-        Integer getsMaxSize = getsMaxSize();
+        int startingSize = getsStartingSize();
+        int maxSize = getsMaxSize(startingSize);
 
-        if (userInput.startsWith("a") || userInput.startsWith("A")) {
-            ArrayList<Integer> testArrayQueue = new ArrayList<Integer>();
-            increaseUntilMaxforList(testArrayQueue, getsStartingSize, getsStartingSize);
-        } else if (userInput.startsWith("l") || userInput.startsWith("L")) {
-            LinkedList<Integer> testLinkedList = new LinkedList<Integer>();
-            increaseUntilMaxforList(testLinkedList, getsStartingSize, getsStartingSize);
-        } else {
-            ComboList<Integer> testComboList = new ComboList<Integer>(10);
-            increaseUntilMaxforCombo(testComboList, getsStartingSize, getsMaxSize());
-        }
-        scanner.close();
+        ArrayList<Integer> testArrayQueue = new ArrayList<Integer>();
+        increaseUntilMaxforList(testArrayQueue, startingSize, maxSize);
+        LinkedList<Integer> testLinkedList = new LinkedList<Integer>();
+        increaseUntilMaxforList(testLinkedList, startingSize, maxSize);
+        ComboList<Integer> testComboList = new ComboList<Integer>(10);
+        increaseUntilMaxforCombo(testComboList, startingSize, maxSize);
+
     }
 
-
-    public static String getsUserInput() {
-        Scanner scanner = new Scanner(System.in);
-        String linkedArrayorCombo = "";
-        linkedArrayorCombo.toLowerCase();
-
-        while (!linkedArrayorCombo.startsWith("a") && !linkedArrayorCombo.startsWith("l") && !linkedArrayorCombo.startsWith("c")) {
-            System.out.println("(A)rray, (L)inked, or (C)ombo Implementation: ");
-            linkedArrayorCombo = scanner.nextLine();
-            
-
-            if (!linkedArrayorCombo.startsWith("a") && !linkedArrayorCombo.startsWith("l") && !linkedArrayorCombo.startsWith("c")) {
-            System.out.println("Invalid input. Please enter a valid letter or word.");
-            }
-        }
-        return linkedArrayorCombo;
-    }
-
-    public static Integer getsStartingSize() {
+    public static Integer getsStartingSize() {   
         System.out.println("What would you like for the structure to start at: ");
-
-        while (!scanner.hasNextInt()) {
-            System.out.println("Invalid input. Please enter a valid number.");
-            scanner.nextLine();
-        }  
-
-        Integer startingSize = scanner.nextInt();
+        int startingSize = scanner.nextInt(); 
         scanner.nextLine();
-        return startingSize;
 
+        if (startingSize < 1) {
+            System.out.println("You have input a negative number or a zero");
+            return getsStartingSize();
+        }
+        return startingSize;
     }
 
-    public static Integer getsMaxSize() {
-        System.out.println("What is your maximum size: ");
-
-        while (!scanner.hasNextInt()) { //how do i make sure that the starting size is less than max size if max size is defined here
-            System.out.println("Invalid input. Please enter a valid number LESS THAN your starting size.");
-            scanner.nextLine();
-
-        }
-
-        Integer doublingSizeUntil = scanner.nextInt();
+    public static Integer getsMaxSize(int startingSize) {
+        System.out.println("What is your maximum size: "); 
+        int maxSize = scanner.nextInt();
         scanner.nextLine();
-        return doublingSizeUntil;
-        
+    
+        if (maxSize <= startingSize) {
+            System.out.println("You have input a number smaller than your starting size, please try again");
+            return getsMaxSize(startingSize);
+        }
+        return maxSize;
     }
 
     public static void increaseUntilMaxforList(List<Integer> L, Integer getsStartingSize, Integer getsMaxSize) {
